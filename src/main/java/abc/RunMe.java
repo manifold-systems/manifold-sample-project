f78503e92b72ca766cd647dd64b35b8f14a0e422
scott <rsmckinney@hotmail.com>
2019-01-07 22:25:36 -0800

    minor

--- src/main/java/abc/RunMe.java
+++ src/main/java/abc/RunMe.java
@@ -47,20 +47,20 @@ public class RunMe {
   }
 
   private static void useImage() {
-    System.out.println("\n\n### Use Image Type Manifold ###\n");
+    out.println("\n\n### Use Image Type Manifold ###\n");
     logo_png logoImage = logo_png.get();
     out.println(logoImage.getIconWidth());
   }
 
   private static void useProperties() {
-    System.out.println("\n\n### Use Properties Type Manifold ###\n");
+    out.println("\n\n### Use Properties Type Manifold ###\n");
     out.println(MyProperties.Chocolate);
     out.println(MyProperties.Chocolate.dark);
     out.println(MyProperties.Chocolate.milk);
   }
 
   private static void useJsonSample() {
-    System.out.println("\n\n### Use JSON Sample Type Manifold ###\n");
+    out.println("\n\n### Use JSON Sample Type Manifold ###\n");
     Person person = Person.create();
     person.setName("Bob");
     person.setAddress(Person.Address.create());
@@ -71,7 +71,7 @@ public class RunMe {
   }
 
   private static void useJsonSchema() {
-    System.out.println("\n\n### Use JSON Schema Type Manifold ###\n");
+    out.println("\n\n### Use JSON Schema Type Manifold ###\n");
     Contact contact = Contact.create();
     contact.setName("Bob");
     contact.setPrimaryAddress(Contact.Address.create("111 Main St.", "Cupertino", "CA"));
@@ -82,20 +82,20 @@ public class RunMe {
   }
 
   private static void useCustomExtension() {
-    System.out.println("\n\n### Use Custom Extension Method ###\n");
+    out.println("\n\n### Use Custom Extension Method ###\n");
     String hello = "hello";
     hello.echo();
   }
 
   private static void useProvidedExtension() {
-    System.out.println("\n\n### Use Collections Extension Library ###\n");
+    out.println("\n\n### Use Collections Extension Library ###\n");
     Iterable<Integer> list = Arrays.asList(1, 2, 3);
     out.println(list.first());
     out.println(list.joinToString(", "));
   }
 
   private static void useStructuralInterface() {
-    System.out.println("\n\n### Use Structural Interfaces ###\n");
+    out.println("\n\n### Use Structural Interfaces ###\n");
     // No casting necessary, Point indirectly implements Coordinate via extension (see MyPointExt)
     Coordinate coord = new Point(4, 5);
     out.println("x: " + coord.getX() + ", y: " + coord.getY());
@@ -107,17 +107,17 @@ public class RunMe {
 
   private static void useSelfType()
   {
-    System.out.println("\n\n### Use Self Type ###\n");
+    out.println("\n\n### Use Self Type ###\n");
     CarBuilder carBuilder = new CarBuilder();
     Car car = carBuilder
       .withName("Mach Five") // returns CarBuilder
       .withColor(255, 255, 255)
       .build();
-    System.out.println(car.getName());
+    out.println(car.getName());
   }
 
   private static void useJailbreak() {
-    System.out.println("\n\n### Use Jailbreak (Type-safe Reflection) ###\n");
+    out.println("\n\n### Use Jailbreak (Type-safe Reflection) ###\n");
     @Jailbreak SampleClass sample = new SampleClass();
     out.println(sample.privateMethod());
     sample._privateField = "assign to private field";
@@ -126,15 +126,15 @@ public class RunMe {
     out.println(sample._privateField);
 
     String data = new SampleClass().jailbreak().privateMethod();
-    System.out.println(data);
+    out.println(data);
 
     @Jailbreak LocalTime time = LocalTime.now();
     time.hour = 10; // private field
-    System.out.println(time);
+    out.println(time);
   }
 
   private static void useStringLiteralTemplates() {
-    System.out.println("\n\n### Use String Literal Templates ###\n");
+    out.println("\n\n### Use String Literal Templates ###\n");
     int hour = 5;
     int minute = 30;
     String nice = "The time is $hour:$minute";
@@ -146,8 +146,8 @@ public class RunMe {
   }
   
   private static void useTemplateManifold() {
-    System.out.println("\n\n### Use ManTL (Type-safe Templates) ###\n");
+    out.println("\n\n### Use ManTL (Type-safe Templates) ###\n");
     String html = SampleTemplate.render("ZOMG");
-    System.out.println(html);
+    out.println(html);
   }
 }
