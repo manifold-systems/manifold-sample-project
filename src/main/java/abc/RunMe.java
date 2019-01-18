8fb423d81f5c5f6f5ca3b96060b66ee9b388fa65
scott <rsmckinney@hotmail.com>
2019-01-17 18:25:01 -0800

    - use latest manifold release (0.38-alpha) - minor changes

--- src/main/java/abc/RunMe.java
+++ src/main/java/abc/RunMe.java
@@ -75,8 +75,8 @@ public class RunMe {
     out.println("\n\n### Use JSON Schema Type Manifold ###\n");
     Contact contact = Contact.builder()
       .withName("Scott McKinney")
+      .withDateOfBirth(LocalDate.of(1986, 8, 9))
       .withPrimaryAddress(Contact.Address.create("111 Main St.", "Cupertino", "CA")).build();
-    contact.setDateOfBirth(LocalDate.of(1986, 8, 9));
     Contact.Address primaryAddress = contact.getPrimaryAddress();
     primaryAddress.setCity("Cupertino");
     primaryAddress.setState("CA");
