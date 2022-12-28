4e205a232b9fa88474a5bd72c750a67b963595c5
scott <rsmckinney@hotmail.com>
2022-12-27 23:52:36 -0500

    use latest manifold

--- src/main/java/abc/stuff/Coordinate.java
+++ src/main/java/abc/stuff/Coordinate.java
@@ -6,53 +6,10 @@ import manifold.ext.rt.api.Structural;
 import java.awt.*;
 
 /**
- * A structural interface defining an (x, y) Coordinate. Coordinate directly implements proxies for Point and Shape,
- * other structurally compatible types are dynamically proxied.
+ * A structural interface defining an (x, y) Coordinate.
  */
-@Structural(factoryClass = Coordinate.ProxyFactory.class)
+@Structural
 public interface Coordinate {
   double getX();
   double getY();
-
-  /**
-   * This proxy factory directly proxies Point and Shape, other types structurally compatible with Coordinate are
-   * _dynamically_ proxied.
-   * <p/>
-   * Note, this proxy factory is optional, it's purpose is to avoid the performance impact of dynamic proxies, which
-   * require dynamic generation, compilation, and loading of Java classes. While dynamic proxies are powerful, the
-   * power comes at the cost of increased runtime footprint to host dynamic Manifold, which includes tools.jar for
-   * Java 8; and the cost of initialization time for each new proxy type to generate and compile. Here, since we know
-   * ahead of time we are using Point and Rectangle in the RunMe class, we opt to implement the proxies statically right
-   * here.
-   * <p/>
-   * Another option involves implementing Coordinate via Extension Classes. As such, abstract extension classes for
-   * Point and Shape would each declare they implement Coordinate and provide static extension methods for getX() and
-   * getY(). In this case proxy factory classes are generated automatically at compile-time.
-   */
-  class ProxyFactory implements IProxyFactory<Object, Coordinate> {
-    @Override
-    public Coordinate proxy(Object o, Class<Coordinate> aClass) {
-      if(o instanceof Point) {
-        return new Coordinate() {
-          public double getX() {
-            return ((Point)o).x;
-          }
-          public double getY() {
-            return ((Point)o).y;
-          }
-        };
-      }
-      else if(o instanceof Shape) {
-        return new Coordinate() {
-          public double getX() {
-            return ((Shape)o).getBounds().x;
-          }
-          public double getY() {
-            return ((Shape)o).getBounds().y;
-          }
-        };
-      }
-      return null;
-    }
-  }
 }
--- src/main/java/manifoldsampleproject/extensions/java/lang/String/MyStringExt.java
+++ src/main/java/manifoldsampleproject/extensions/java/lang/String/MyStringExt.java
@@ -1,6 +1,7 @@
 package manifoldsampleproject.extensions.java.lang.String;
 
 import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.Jailbreak;
 import manifold.ext.rt.api.This;
 
 import java.lang.String;
@@ -9,5 +10,10 @@ import java.lang.String;
 public class MyStringExt {
   public static void echo( @This String thiz) {
     System.out.println(thiz);
+  }
+
+  /** Yikes!  Don't try this at home! */
+  public static void setBytes(@This @Jailbreak String thiz, byte[] bytes) {
+    thiz.value = bytes;
   }
 }
